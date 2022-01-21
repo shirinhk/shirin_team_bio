8286521f3693c5c737444475dadbaca9b454fab5
Shirin Hk <shirinhk@Shirins-MacBook-Pro.local>
2022-01-21 12:12:43 -0500

    fix port issue for Heroku

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-//const http = require('http');
 import { createServer } from 'http';
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const server = createServer((req, res) => {
   res.statusCode = 200;
@@ -10,6 +8,6 @@ const server = createServer((req, res) => {
   res.end('Hello World');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server running at ${port}/`);
 });
